8863c81be2604c143d2d6c10ab64ee2786f3d884
mariavitoriadsm26 <mariavitoriadsm@gmail.com>
2025-07-30 13:54:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ function inserirNome() {
 }
 
 inserirNome();
+//Lista de linguagens escritas:
+
+let linguagens ["javaScript","Python", "C"];
+
+console.log(linguagens[0]) //"JavaScript"
+console.log(linguagens[1]) //"Python"
+console.log(linguagens)[2]) //"C"
+
+const lista = document.querySelector("#lista);
+lista.textContent = linguagens[0];
+lista.textContent = linguagens[1];
+lista.textContent = linguagens[2];
